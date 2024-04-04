ca73dbfcdbf9074d6e02a829697a1858c6da7cca
egatest <165974470+egatest@users.noreply.github.com>
2024-04-03 22:03:50 -0400

    Update test_store.py

--- test_store.py
+++ test_store.py
@@ -1,6 +1,8 @@
 from jsonschema import validate
 import pytest
 import schemas
+import json
+import random
 import api_helpers
 from hamcrest import assert_that, contains_string, is_
 
@@ -12,5 +14,43 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+
+
+@pytest.fixture
+def get_random_pet_ids():
+    pet_id_set = [0, 1, 2]
+    pet_id = random.choice(pet_id_set)
+    print("pet_id-->", pet_id)
+    return pet_id
+
+
+def post_an_order_to_get_pet(pet_id):
+  test_endpoint = "/store/order"
+  data = {
+     "pet_id": pet_id
+     } 
+  response = api_helpers.post_api_data(test_endpoint, data)
+  print("post_api_data_response", response)
+  assert response.status_code == 201
+  if response.status_code == 201:
+        validate(response.json(), schemas.order)
+  order_id = response.json().get('id')
+  return order_id
+
+
+# My Function that tests the PATCH request /store/order/{order_id}
+def test_patch_order_by_id(get_random_pet_ids):
+    pet_id = get_random_pet_ids # getting the random pet ids through fixture
+    print("pet_id",pet_id) 
+    # Getting the order id from the post operation
+    order_id = post_an_order_to_get_pet(pet_id)
+    print("post_order_response", order_id)
+    # concatenating the order id to the patch url
+    test_endpoint = "/store/order/" + order_id
+    data = {
+        "status": "available"
+        } 
+    response = api_helpers.patch_api_data(test_endpoint, data)
+    assert response.status_code == 200
+    assert response.json().get("message") == 'Order and pet status updated successfully'
+    
