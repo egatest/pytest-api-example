621f11a97e4cf092234ed5bfbb48b941cd5f51c0
egatest <165974470+egatest@users.noreply.github.com>
2024-04-03 22:03:37 -0400

    Update test_pet.py

--- test_pet.py
+++ test_pet.py
@@ -9,6 +9,10 @@ TODO: Finish this test by...
 1) Troubleshooting and fixing the test failure
 The purpose of this test is to validate the response matches the expected schema defined in schemas.py
 '''
+
+
+# My Response -> Incorrect schema data type (Number) is given to the pet name instead of string
+
 def test_pet_schema():
     test_endpoint = "/pets/1"
 
@@ -26,7 +30,7 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+@pytest.mark.parametrize("status", [("available"),("sold"),("pending")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
@@ -34,7 +38,14 @@ def test_find_by_status_200(status):
     }
 
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+    # My Response -> Asserting the success status code
+    assert response.status_code == 200 
+    # My Response -> Validating the response received for all the three status
+    # My Response -> Iterating through each pet via for loop rather than map() since we are not returning anything rather than asserting the status
+    for singlePet in response.json():
+        assert singlePet["status"] == status
+        validate(instance=singlePet, schema=schemas.pet) 
+
 
 '''
 TODO: Finish this test by...
@@ -42,5 +53,10 @@ TODO: Finish this test by...
 2) Parameterizing the test for any edge cases
 '''
 def test_get_by_id_404():
-    # TODO...
-    pass
+    # My Response -> there are 3 pets in total number and we are searching for a unknown pet with id = 3
+    test_endpoint = "/pets/3"
+    response = api_helpers.get_api_data(test_endpoint)
+    assert response.status_code == 404
+
+
+
